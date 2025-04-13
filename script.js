51f56f95dd5bc356093b3b6de6477be4d7259e7f
Ridhcheall Doherty <132768875+hillister@users.noreply.github.com>
2025-04-13 18:37:28 +0100

    log temperature, and short description of weather

--- script.js
+++ script.js
@@ -4,10 +4,12 @@ async function getCityWeather() {
         const apiKey = 'b69617f26cc0a63f2dc4a1d0d935f6fe';
         const weatherApi = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`)
         const weatherData = await weatherApi.json()
-
         console.log(weatherData)
+        console.log(city)
+        console.log(weatherData.main.temp, 'Celsius')
+        console.log(weatherData.weather[0].description)
     } catch(err){
-        console.error('Something went wrong', err)
+        console.error('City not found', err)
     }
 }
 

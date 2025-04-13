aee9b5f6744d909c4499e9379132de75c84f8d5b
Ridhcheall Doherty <132768875+hillister@users.noreply.github.com>
2025-04-13 17:56:52 +0100

    added weather api and displays in console the data of city inputed by user

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 async function getCityWeather() {
     try {
         const city = document.querySelector('input').value
-        console.log(city)
+        const apiKey = 'b69617f26cc0a63f2dc4a1d0d935f6fe';
+        const weatherApi = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`)
+        const weatherData = await weatherApi.json()
+
+        console.log(weatherData)
     } catch(err){
         console.error('Something went wrong', err)
     }
@@ -10,3 +14,4 @@ async function getCityWeather() {
 document.querySelector('button').addEventListener('click', () => {
     getCityWeather()
 })
+
